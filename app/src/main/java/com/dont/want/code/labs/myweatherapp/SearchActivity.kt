33000b98d7abb258e1a3e163c3cdf2bf8878ac0c
Glubshev Artem <36304382+SomeAnonimCoder@users.noreply.github.com>
2021-10-03 17:34:49 +0300

    Added city list from openweathermap(cities.json) Loading this file works

--- app/src/main/java/com/dont/want/code/labs/myweatherapp/SearchActivity.kt
+++ app/src/main/java/com/dont/want/code/labs/myweatherapp/SearchActivity.kt
@@ -1,11 +1,72 @@
 package com.dont.want.code.labs.myweatherapp
 
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.ProgressBar
+import androidx.recyclerview.widget.RecyclerView
+import org.json.JSONArray
+
 
 class SearchActivity : AppCompatActivity() {
+
+    lateinit var progressBar: ProgressBar
+    lateinit var searchContainer:LinearLayout
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+
+        val searchTextEdit = findViewById<EditText>(R.id.city_search)
+        val cityRecyclerView = findViewById<RecyclerView>(R.id.city_list)
+
+        searchContainer = findViewById<LinearLayout>(R.id.search_container)
+        progressBar = findViewById<ProgressBar>(R.id.progress_bar)
+
+            searchTextEdit.addTextChangedListener(object : TextWatcher {
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                // TODO filter recycleview
+            }
+        })
+
+
     }
+
+    inner class fileLoader():AsyncTask<Void, Void, JSONArray>(){
+
+        override fun onPreExecute() {
+            super.onPreExecute()
+            progressBar.visibility = View.VISIBLE
+            searchContainer.visibility = View.GONE
+        }
+
+        override fun doInBackground(vararg p0: Void?): JSONArray {
+            val citiesFile = resources.openRawResource(R.raw.cities)
+            val jsonString = citiesFile.bufferedReader().readText()
+            val citiesList = JSONArray(jsonString)
+            return  citiesList
+        }
+
+        override fun onPostExecute(result: JSONArray?) {
+            super.onPostExecute(result)
+            progressBar.visibility = View.GONE
+            searchContainer.visibility = View.VISIBLE
+        }
+
+    }
+
 }
