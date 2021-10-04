55aa100157cd41f5bbae05092d2c1bc850935463
Glubshev Artem <36304382+SomeAnonimCoder@users.noreply.github.com>
2021-10-04 23:30:17 +0300

    Daily forecast works

--- app/src/main/java/com/dont/want/code/labs/myweatherapp/ForecastActivity.kt
+++ app/src/main/java/com/dont/want/code/labs/myweatherapp/ForecastActivity.kt
@@ -32,7 +32,8 @@ class ForecastActivity : AppCompatActivity() {
     var cityName:String = "Nowhere"
     var country:String = "Nowhere"
     val apiKey:String = "42af006c49cad6fb2ae56af9fd967928"
-    lateinit var hourlyRecyclerView:RecyclerView
+    lateinit var hourlyForecastRecyclerView:RecyclerView
+    lateinit var dailyForecastRecyclerView:RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -70,23 +71,23 @@ class ForecastActivity : AppCompatActivity() {
             }
         }
 
-        hourlyRecyclerView = findViewById<RecyclerView>(R.id.hourly_recycler_view)
-        hourlyRecyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        hourlyForecastRecyclerView = findViewById<RecyclerView>(R.id.hourly_recycler_view)
+        hourlyForecastRecyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+
+        dailyForecastRecyclerView = findViewById<RecyclerView>(R.id.daily_recycler_view)
+        dailyForecastRecyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+
 
 
         Lat = this.intent.getDoubleExtra("lat", 0.0)
         Lon = this.intent.getDoubleExtra("lon", 0.0)
         cityName = this.intent.getStringExtra("city_name")!!
         country = this.intent.getStringExtra("country_name")!!
-        forecastLoader().execute()
+        forecastDataLoader().execute()
     }
 
     // TODO deprecated, find another method
-    inner class forecastLoader : AsyncTask<String, Void, String>(){
-
-        override fun onPreExecute() {
-            super.onPreExecute()
-        }
+    inner class forecastDataLoader : AsyncTask<String, Void, String>(){
 
         override fun doInBackground(vararg p0: String?): String {
             var response:String?
@@ -130,15 +131,58 @@ class ForecastActivity : AppCompatActivity() {
             try {
                 val hourly = result.getJSONArray("hourly")
                 val hourlyDataSet = getHourlyDataset(hourly)
-                hourlyRecyclerView.adapter = HourlyForecastAdapter(hourlyDataSet)
-                hourlyRecyclerView.setHasFixedSize(true)
+                hourlyForecastRecyclerView.adapter = HourlyForecastAdapter(hourlyDataSet)
+                hourlyForecastRecyclerView.setHasFixedSize(true)
             }
             catch (e:Exception){
                 Log.println(Log.DEBUG, "HOUR_ERR", e.localizedMessage)
 
             }
 
-            val daily = result.getJSONArray("daily")
+            val dailyJSON = result.getJSONArray("daily")
+            val dailyDataset = detDailyDataset(dailyJSON)
+            dailyForecastRecyclerView.adapter = DailyForecastAdapter(dailyDataset)
+            hourlyForecastRecyclerView.setHasFixedSize(true)
+        }
+
+
+
+        fun detDailyDataset(daily:JSONArray):ArrayList<DailyDataPoint>{
+            val result = ArrayList<DailyDataPoint>()
+            for(i in 0 until daily.length()){
+                val element = daily.getJSONObject(i)
+
+                val time = Date(element.getLong("dt")*1000)
+
+                val temp = element.getJSONObject("temp")
+                val mourning = temp.getDouble("morn")
+                val day = temp.getDouble("day")
+                val evening = temp.getDouble("eve")
+                val night = temp.getDouble("night")
+
+                val fl = element.getJSONObject("feels_like")
+                val mourning_fl = fl.getDouble("morn")
+                val day_fl = fl.getDouble("day")
+                val evening_fl = fl.getDouble("eve")
+                val night_fl = fl.getDouble("night")
+
+                val pressure = element.getInt("pressure")
+                val wind = element.getDouble("wind_speed")
+                val humidity = element.getDouble("humidity")
+                val clouds = element.getDouble("clouds")
+                val status = element.getJSONArray("weather").getJSONObject(0).getString("description")
+
+                val sunset = Date(element.getLong("sunset")*1000)
+                val sunrise = Date(element.getLong("sunrise")*1000)
+
+                val dp = DailyDataPoint(time,
+                    mourning, day, evening, night,
+                    mourning_fl, day_fl, evening_fl, night_fl,
+                    pressure, wind, humidity, clouds, status, sunset, sunrise
+                )
+                result.add(dp)
+            }
+            return result
         }
 
         fun getHourlyDataset(hourly: JSONArray): ArrayList<HourlyDataPoint> {
@@ -219,8 +263,6 @@ class ForecastActivity : AppCompatActivity() {
             val wind = dataPoint.wind
             viewHolder.itemView.findViewById<TextView>(R.id.wind).text = wind.toString()+" m/s"
 
-
-
         }
 
         // Return the size of your dataset (invoked by the layout manager)
@@ -228,7 +270,81 @@ class ForecastActivity : AppCompatActivity() {
 
     }
 
-    data class HourlyDataPoint(val time:Date, val cityName:String, val country:String, val temp:Double, val humidity:Double, val status:String,val wind:Double)
+    class DailyForecastAdapter(private val dailyDataset: ArrayList<DailyDataPoint>) : RecyclerView.Adapter<DailyForecastAdapter.ViewHolder>() {
+        class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {}
 
+        // Create new views (invoked by the layout manager)
+        override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+            // Create a new view, which defines the UI of the list item
+            val view = LayoutInflater.from(viewGroup.context)
+                .inflate(R.layout.dayly_forecast_item, viewGroup, false)
+
+            return DailyForecastAdapter.ViewHolder(view)
+        }
+
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+            val v = holder.itemView
+            val dateFormatter = SimpleDateFormat("dd MMM", Locale.ENGLISH)
+            val timeFormatter = SimpleDateFormat("HH:mm", Locale.ENGLISH)
+
+
+            val dp = dailyDataset[position] as DailyDataPoint
+            val time = dp.time
+            v.findViewById<TextView>(R.id.time).text = dateFormatter.format(time)
+
+            val morning = dp.morning
+            v.findViewById<TextView>(R.id.morning).text = morning.toString() + "°C"
+            val day = dp.day
+            v.findViewById<TextView>(R.id.day).text = day.toString() + "°C"
+            val evening = dp.evening
+            v.findViewById<TextView>(R.id.evening).text = evening.toString() + "°C"
+            val night = dp.night
+            v.findViewById<TextView>(R.id.night).text = night.toString() + "°C"
+
+            val morning_fl = dp.morning_fl
+            v.findViewById<TextView>(R.id.morning_sl).text = morning_fl.toString() + "°C"
+            val day_fl = dp.day_fl
+            v.findViewById<TextView>(R.id.day_sl).text = day_fl.toString() + "°C"
+            val evening_fl = dp.evening_fl
+            v.findViewById<TextView>(R.id.evening_sl).text = evening_fl.toString() + "°C"
+            val night_fl = dp.night_fl
+            v.findViewById<TextView>(R.id.night_sl).text = night_fl.toString() + "°C"
+
+            val pressure = dp.pressure
+            v.findViewById<TextView>(R.id.pressure).text = pressure.toString() + "gPa"
+            val wind = dp.wind
+            v.findViewById<TextView>(R.id.wind).text = wind.toString() + "m/s"
+            val humidity = dp.humidity
+            v.findViewById<TextView>(R.id.humidity).text = humidity.toString() + " %"
+            val clouds = dp.clouds
+            v.findViewById<TextView>(R.id.clouds).text = clouds.toString() + " %"
+            val status = dp.status
+            v.findViewById<TextView>(R.id.status).text = status
+
+            val sunset = dp.sunset
+            v.findViewById<TextView>(R.id.sunset).text = timeFormatter.format(sunset)
+            val sunrise = dp.sunrise
+            v.findViewById<TextView>(R.id.sunrise).text = timeFormatter.format(sunrise)
+
+
+        }
+
+        // Return the size of your dataset (invoked by the layout manager)
+        override fun getItemCount() = dailyDataset.size
+
+    }
+
+
+    data class HourlyDataPoint(val time:Date, val cityName:String, val country:String,
+                               val temp:Double, val humidity:Double, val status:String,
+                               val wind:Double)
+
+    data class DailyDataPoint(val time: Date,
+                              val morning:Double, val day:Double, val evening:Double, val night:Double,
+                              val morning_fl:Double, val day_fl:Double, val evening_fl:Double, val night_fl:Double,
+                              val pressure:Int, val wind:Double, var humidity:Double, val clouds:Double,
+                              val status: String, val sunset:Date, val sunrise:Date)
 
 }
